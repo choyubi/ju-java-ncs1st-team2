ee702f8c762a009d85bdf9c83c6c31ae0242f05f
jiyousub <qkwmf91@naver.com>
2016-11-29 13:13:37 +0900

    DocumentWriteOK Servelt추가

--- ssc/src/com/ssc/service/impl/FileServiceImpl.java
+++ ssc/src/com/ssc/service/impl/FileServiceImpl.java
@@ -2,10 +2,23 @@ package com.ssc.service.impl;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.logging.log4j.Logger;
+
 import com.ssc.model.File;
 import com.ssc.service.FileService;
 
 public class FileServiceImpl implements FileService {
+	
+	/** 처리 결과를 기록할 Log4J 객체 생성 */
+	Logger logger;
+	/** MyBatis */
+	SqlSession sqlSession;
+	
+	public FileServiceImpl(SqlSession sqlSession, Logger logger) {
+		this.sqlSession = sqlSession;
+		this.logger = logger;
+	}
 
 	@Override
 	public void insertFile(File file) throws Exception {
